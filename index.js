57eff92109f685bba36a39bd3393f8ca68db4b9c
GLOIRE MUTALIKO <mufunyig@gmail.com>
2022-12-30 14:42:03 +0100

    feat: db config connexion

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ import express from "express";
 
 const app = express();
 
+//Middlewares
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+//Routes
+
 
 app.listen(8000);
